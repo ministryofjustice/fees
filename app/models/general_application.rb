01cb924fb9501c1998e0a5605f88e31cfcf347ae
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-01-28 11:49:41 +0000

    friendly_id added to the model
    And the appropriate specs added to confirms that it works as expected.

--- app/models/general_application.rb
+++ app/models/general_application.rb
@@ -1,4 +1,7 @@
 class GeneralApplication < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   validates :title, presence: true
   validates :fee, presence: true
 end
--- spec/models/general_application_spec.rb
+++ spec/models/general_application_spec.rb
@@ -2,36 +2,49 @@ require 'rails_helper'
 
 RSpec.describe GeneralApplication, :type => :model do
 
-  before(:each) { application.valid? }
+  context 'validations' do
+    before(:each) { application.valid? }
 
-  describe 'correct general application' do
-    let(:application) do
-      GeneralApplication.new(title: 'application1',
-                             fee: '1')
+    describe 'correct general application' do
+      let(:application) do
+        GeneralApplication.new(title: 'application1',
+                               fee: '1')
+      end
+
+      it 'should be valid' do
+        expect(application).to be_valid
+      end
     end
 
-    it 'should be valid' do
-      expect(application).to be_valid
+    describe '#title' do
+      context 'when only the title is present' do
+        let(:application) { GeneralApplication.new(title: 'application1') }
+
+        it 'should not be valid' do
+          expect(application).to_not be_valid
+        end
+      end
     end
-  end
 
-  describe '#title' do
-    context 'when only the title is present' do
-      let(:application) { GeneralApplication.new(title: 'application1') }
+    describe '#fee' do
+      context 'when only the fee is present' do
+        let(:application) { GeneralApplication.new(fee: '1') }
 
-      it 'should not be valid' do
-        expect(application).to_not be_valid
+        it 'should not be valid' do
+          expect(application).to_not be_valid
+        end
       end
     end
   end
 
-  describe '#fee' do
-    context 'when only the fee is present' do
-      let(:application) { GeneralApplication.new(fee: '1') }
+  describe 'slug generation' do
+    before { GeneralApplication.create!(title: 'application1', fee: '1') }
 
-      it 'should not be valid' do
-        expect(application).to_not be_valid
-      end
+    let(:last_entry) { GeneralApplication.last }
+    let(:friendly) { GeneralApplication.friendly.find('application1') }
+
+    it 'should find the general application based on the slug id' do
+      expect(last_entry).to eq friendly
     end
   end
 end
