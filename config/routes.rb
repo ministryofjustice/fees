93ba94621d15ec6379a29e1e72758d7dc434a3ee
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-02-09 11:39:10 +0000

    Changed the routing syntax
    So that it fits with the rest of the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'general_applications' => 'general_applications#index'
   get 'general_applications/:id', to: 'general_applications#show'
 
-  get 'categories' => 'fee_categories#index'
+  get 'categories', to: 'fee_categories#index', as: 'categories'
   get 'categories/:id', to: 'fee_categories#show', as: 'category'
 end
