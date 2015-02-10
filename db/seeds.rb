8ecdca9eb360134ee07e76f68de6d73120ce514c
Александар Симић <asimic@gmail.com>
2015-02-10 14:36:47 +0000

    Basic seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+FeeCategory.create!([
+  {title: "Civil Claims"},
+  {title: "Bankruptcy"},
+  {title: "General Applications"}
+])
+
+FeeType.create!([
+  {fee_category_id: 1, title: "Entering a petition to declare yourself bankrupt (debtor’s petition)", amount: 180},
+  {fee_category_id: 1, title: "Entering a petition to make someone who owes you money bankrupt (creditor’s petition)", amount: 280},
+  {fee_category_id: 1, title: "Entering a winding-up petition (companies only)", amount: 280},
+  {fee_category_id: 1, title: "Any other petition where no other fee is specified", amount: 280},
+  {fee_category_id: 2, title: "High Court claim", amount: 480},
+  {fee_category_id: 2, title: "County Court Claim", amount: 280},
+  {fee_category_id: 2, title: "Possession Claims Online (PCOL)", amount: 250},
+  {fee_category_id: 3, title: "Application on notice where no other fee is specified", amount: 150}
+])
