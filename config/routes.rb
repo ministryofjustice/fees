58472d57192dac8b13bfe951147b976da8668a70
Александар Симић <asimic@gmail.com>
2015-02-11 09:42:35 +0000

    Removed left over old routes
    Removed the routes for the general_application model/controller, which
    is no longer used.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'general_applications' => 'general_applications#index'
-  get 'general_applications/:id', to: 'general_applications#show'
 
   get 'categories', to: 'fee_categories#index', as: 'categories'
   get 'categories/:id', to: 'fee_categories#show', as: 'category'
